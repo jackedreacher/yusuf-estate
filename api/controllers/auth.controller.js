5ec2631d2756cb3c14a89f8486ab52ccb724a2da
jackedreacher <nure234ttin@gmail.com>
2024-03-25 04:07:32 +0300

    Created a Sign In API for NodeJS Backend using JSON Web Token

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 // to to  crypto the password at mongodb
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +11,24 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("newUser created");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const singin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(400, "User  not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc;
+
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
